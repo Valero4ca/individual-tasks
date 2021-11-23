ecad065da89c163d6052560672524bc1245daa95
Valeria Makarevich <valeriamakarevich@outlook.com>
2021-11-23 19:09:13 +0300

    Home

--- Pointers/Pointers.cpp
+++ Pointers/Pointers.cpp
@@ -10,12 +10,34 @@ int function(int *array_pointer) {
 int main()
 {
     setlocale(LC_ALL, "RUS");
-    int a = 11;
-    int* pointer;
-    pointer = &a;
-    std::cout << "Обращаясь к указателю мы получаем адрес переменной 'a': " << pointer << std::endl;
-    std::cout << "Обращаясь к указателю по пдресу получаем значение в переменной 'a': " << *pointer << std::endl;
-    int *array = new int[10];
-    function(array);
-    std::cout << array[0];
+    int a = 10;
+    int *pol = &a;
+    std::cout << "а: " << a <<std::endl;
+    std::cout << "а: " << &a << std::endl;
+    std::cout << "а: " << pol << std::endl;
+    std::cout << "а: " << &pol << std::endl;
+    std::cout << "а: " << pol << std::endl;
+    std::cout << "а: " << *pol << std::endl;
+
+    int *fol = new int[10];
+
+    for (int a = 0; a < 10; a++) {
+        fol[a] = 0;
+    }
+    std::cout << fol << std::endl;
+    std::cout << &fol[0] << std::endl;
+    std::cout << fol[0] << std::endl;
+
+    int tet[90];
+    int i = 0;
+    while (i < 10) {
+        i++;
+    }
+    for (int m = 0; m < 90; m++) {
+        tet[m] = m;
+    }
+    std::cout << tet << std::endl;
+    std::cout << &tet[0] << std::endl;
+    std::cout << tet[89] << std::endl;
+
 }
