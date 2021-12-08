033cf89387f6012a10ccab30e557cf083fd83bc1
Valeria Makarevich <valeriamakarevich@outlook.com>
2021-12-08 12:06:23 +0300

    контрольная работа первое задание

---
+++ Контрольная работа !/Контрольная работа !.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <math.h>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+
+    setlocale(LC_ALL, "Russian");
+    float a, y, x, h, b;
+    std::cout << "Введите значение h\n";
+    std::cin >> h;
+    std::cout << "Введенное значение: " << h << std::endl;
+
+    while (a<=b) {
+
+        y = pow((3), x);
+        std::cout << "y(" << a << ")" << " = " << y << std::endl;
+
+        a = a + h;
+    }
+    int max_index;
+    int A[5];
+    max_index = 0;
+
+    for (int b = 1; b < 5, b++) {
+        if (A[max_index] < A[b]) {
+            max_index = b;
+        }
+    }
+
+    return 0;
+}
