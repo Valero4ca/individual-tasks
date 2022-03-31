2278c1dcc4769e3ebd3d72fe214f7b45ae8de96f
Valeria Makarevich <valeriamakarevich@outlook.com>
2022-03-31 20:12:53 +0300

    закинула дз

--- Homework3.4/Homework3.4.cpp
+++ Homework3.4/Homework3.4.cpp
@@ -55,4 +55,6 @@ int main()
 
 	std::cout << std::endl<< "Среднее арифметическое: "<<sr;
 
+	delete[] A;
+	system("pause");
 }
---
+++ Функции 5/Функции 5.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>
+#include <math.h>
+
+
+using namespace std;
+
+double sumer(double x, double eps)
+{
+    
+    double sum = x, term = x;
+    x = x * x;
+    for (int i = 3; fabs(x) / i > eps; i += 2) sum += (term *= x) / i;
+    return 2 * sum;
+}
+
+double uravn(double g, int n)   // Основное уравнение 1
+{
+    double y;
+    y = 1 + (x * x) / 2 * atan(x) * atan(x) - x / 2;
+    return x;                         // значение функции
+}
+
+
+
+
+
+
+void rez(double a, double b, double h, int n) // a - откуда b - до , h - шаг, 
+{
+    double r, k, s, x;
+    for (x = a; x <= b; x += h)
+    {
+        s = r = 1;
+        for (k = 0; k <= n; ++k)
+        {
+            r = (pow(-1,k+1)*(x, 2* k + 1)) / (4* k - 1);
+            s += r;
+        }
+
+    }
+
+    for (x = a; x <= b; x += h)
+    {
+        std::cout << "Результаты вычислений:\n" << "\tx=" << x << "\ty=" << rez(x) << "\ts=" << s << std::endl;
+    }
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    double a, b, r, k, h, s, x, y;
+    int n;
+    // Произвольные значения
+    std::cout << "Задайте переменную a\n";
+   std::cin >> a;
+    std::cout << "Задайте переменную b\n";
+    std::cin >> b;
+    for (;;) // бесконечный цикл
+    {
+        std::cout << "Задайте переменную h\n";
+        std::cin >> h;
+        if (h <= 0)
+        {
+            std::cout << "Неверное значение шага вычислений\n";
+            std::cin.clear();
+            std::cin.ignore(32767, '\n');
+            continue;
+        }
+        else
+        {
+            break;
+        }
+
+    }
+
+    for () // бесконечный цикл
+    {
+        cout << "Задайте переменную n\n";
+        cin >> n;
+        if (n <= 0)
+        {
+            cout << "Неверное значение количества выполнение операций\n";
+            cin.clear();
+            cin.ignore(32767, '\n');
+            continue;
+        }
+        else
+        {
+
+            break;
+        }
+
+    }
+
+    rez(a, b, h, n);
+
+    return 0;
+}
